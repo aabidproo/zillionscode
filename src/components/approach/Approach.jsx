aa41d11e2fc98cc0fbac2d8ed1e897c95236bcc2
Aabid Hasan <aabidhasan495@gmail.com>
2024-05-13 01:37:38 +0545

    .

--- src/components/approach/Approach.jsx
+++ src/components/approach/Approach.jsx
@@ -3,8 +3,37 @@ import './approach.css'
 
 function Approach() {
   return (
-    <div>
-      Approach
+    <div className='approach_container'>
+      <h2>Our Approach</h2>
+      <p>Embark on a journey of enlightenment and innovation at Bodhi Tech. Our tech solutions are designed to empower and propel you into a brighter</p>
+
+      <div className='approach_box_wrapper'>
+        <div className='approach_box'>
+          <h3 className='text_gradient'>01</h3>
+          <line></line>
+          <h4>Discovery</h4>
+          <p>We build websites that not only align with your brand but also one that delivers results aligned with your business goals.</p>
+        </div>
+        <div className='approach_box'>
+          <h3 className='text_gradient' >02</h3>
+          <line></line>
+          <h4>Strategy</h4>
+          <p>We build websites that not only align with your brand but also one that delivers results aligned with your business goals.</p>
+        </div>
+        <div className='approach_box'>
+          <h3 className='text_gradient' >03</h3>
+          <line></line>
+          <h4>Design and Develop</h4>
+          <p>We build websites that not only align with your brand but also one that delivers results aligned with your business goals.</p>
+        </div>
+        <div className='approach_box'>
+          <h3 className='text_gradient' >04</h3>
+          <line></line>
+          <h4>Launch & Optimize</h4>
+          <p>We build websites that not only align with your brand but also one that delivers results aligned with your business goals.</p>
+        </div>
+
+      </div>
     </div>
   )
 }
--- src/components/company/Company.jsx
+++ src/components/company/Company.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import '../company/company.css'
 
+import logo1 from '../../assets/figma.png'
+import logo2 from '../../assets/flutter.png'
+import logo3 from '../../assets/nextjs.png'
+import logo4 from '../../assets/nodejs.png'
+import logo5 from '../../assets/python.png'
 
 function Company() {
   return (
     <div className='company_container'>
       <p>Trusted by Fastest Growing SMEs & Startups</p>
       <div className='company_logo'>
-        <img src='src/assets/figma.png' alt='figma logo'/>
-    
+        <img src={logo1} alt='logo1'/>
+        <img src={logo2} alt='logo1'/>
+        <img src={logo3} alt='logo1'/>
+        <img src={logo4} alt='logo1'/>
+        <img src={logo5} alt='logo1'/>
       </div>
     </div>
   )

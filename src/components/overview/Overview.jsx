a07c8e7fead690e2206fe05df9cdc12ad6c676ec
Aabid Hasan <aabidhasan495@gmail.com>
2024-05-14 02:00:19 +0545

    .

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -3,8 +3,28 @@ import './overview.css'
 
 function Overview() {
   return (
-    <div>
-      Overview
+    <div className='overview_container'>
+      <div className='overview_box_wrapper'>
+        <div className='overview_box'>
+          <h3>2+</h3>
+          <line></line>
+          <h4>Years of Operation</h4>
+          <p>and being strategic partners for our clients</p>
+        </div>
+        <div className='overview_box'>
+          <h3>40+</h3>
+          <line></line>
+          <h4>App and Website Project Completed</h4>
+          <p>On time delivery of projects is key to our success.</p>
+        </div>
+        <div className='overview_box'>
+          <h3>74</h3>
+          <line></line>
+          <h4>Net Promoter Score</h4>
+          <p>Customer satisfaction has always been our priority</p>
+        </div>
+
+      </div>
     </div>
   )
 }
--- src/components/solutions/Solutions.jsx
+++ src/components/solutions/Solutions.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './solutions.css'
+import banner from '../../assets/image03.png'
+
 
 function Solutions() {
   return (
-    <div>
-      Solutions
+    <div className='hero_container'>
+      <div className='hero_wrapper'>
+
+        <div className='hero_content'>
+        Solutions List in toogle style
+        </div>
+
+        <div className='hero_banner'>
+          <img src={banner} alt='banner' />
+        </div>
+
+      </div>
     </div>
   )
 }
